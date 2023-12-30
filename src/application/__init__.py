ec33ef0c77bdf43f2181c55da50ff78003e8e2d8
Igor Szabłowski <iszablow@gmail.com>
2023-12-30 17:53:29 +0100

    sqlite database, user model, and signing up user authorization added

--- src/application/__init__.py
+++ src/application/__init__.py
@@ -1,16 +1,43 @@
 from flask import Flask
-from .auth import auth
-from .user import user
-from .cards import cards
-from .home import home
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app():
 
     app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+    db.init_app(app)
+
+    from .auth import auth
     app.register_blueprint(auth)
+
+    from .user import user
     app.register_blueprint(user)
+
+    from .home import home
     app.register_blueprint(home)
+
+    from .cards import cards
     app.register_blueprint(cards)
+
+    with app.app_context():
+        db.create_all()
+
+    from .models.user_model import User
+
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     return app
 
--- src/application/auth.py
+++ src/application/auth.py
@@ -1,17 +1,37 @@
 from flask import Blueprint, request, url_for, redirect, render_template
+from werkzeug.security import check_password_hash, generate_password_hash
+from .models.user_model import User
+from . import db
 
 auth = Blueprint('auth', __name__, url_prefix='/auth')
 
 @auth.route('/signin', methods=['GET', 'POST'])
-def sign_in():
+def login():
     if request.method == 'GET':
-        return render_template('auth/signin.html')
+        return render_template('auth/login.html')
     
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'GET':
         return render_template('auth/signup.html')
+
+    elif request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        password_confirmation = request.form.get('password-confirmation')
+
+        user = User.query.filter_by(name=username).first()
+
+        if user or password != password_confirmation:
+            return redirect(url_for('auth.sign_up'))
+        
+        new_user = User(name=username, password=generate_password_hash(password))
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('auth.login'))
     
 @auth.route('/logout', methods=['GET'])
 def logout():
