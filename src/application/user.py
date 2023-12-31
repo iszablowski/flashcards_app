ab71a5ce805859bdb19f18d02d86a9a273d4649f
Igor Szabłowski <iszablow@gmail.com>
2023-12-31 16:28:26 +0100

    working on profile templates

--- src/application/user.py
+++ src/application/user.py
@@ -6,11 +6,12 @@ user = Blueprint('user', __name__, url_prefix='/profile')
 @user.route('/', methods=['GET'])
 @login_required
 def user_page():
-    return render_template('base.html', user_name=current_user.name, logged_in=current_user.is_authenticated)
+    return render_template('user/profile.html', user_name=current_user.name, logged_in=current_user.is_authenticated)
 
 @user.route('/settings', methods=['GET', 'POST'])
+@login_required
 def user_settings():
-    return '<h3>settings</h3>'
+    return render_template('user/settings.html', user_name=current_user.name, logged_in=current_user.is_authenticated)
 
 @user.route('/collection', methods=['GET'])
 def user_collection():
