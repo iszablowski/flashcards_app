186a71e891e6009bd221c5d92c5c864c346f4d9d
Igor Szabłowski <iszablow@gmail.com>
2024-01-08 00:38:29 +0100

    new removing method

--- src/application/static/js-scripts/creating_flashcard.js
+++ src/application/static/js-scripts/creating_flashcard.js
@@ -2,6 +2,9 @@ const add_row_btn = document.querySelector('#add-row-btn');
 const flashcard_row = document.querySelector('.flashcard-row');
 const all_flashcards = document.querySelector('.all-flashcards');
 const remove_row_btn = document.querySelector('#remove-row-btn');
+const remove_btn = document.querySelector('#remove-btn');
+
+let new_row_index = 1;
 
 function clear_inputs(inputs) {
     inputs.forEach((input) => {
@@ -12,20 +15,26 @@ function clear_inputs(inputs) {
 function add_row() {
     let new_row = flashcard_row.cloneNode(true);
     clear_inputs(new_row.querySelectorAll('input[type=text]'));
+    new_row.setAttribute('value', new_row_index);
+    let new_button = new_row.querySelector('#remove-btn');
+    new_button.value = new_row_index;
+    new_button.addEventListener('click', remove_row);
     all_flashcards.appendChild(new_row);
-}
-
-function remove_row() {
-    let rows = document.querySelectorAll('.flashcard-row');
-    if (rows.length > 1) {
-        rows[rows.length-1].remove();
-    }
+    new_row_index++;
 }
 
 function scroll_smooth_to_bottom() {
     window.scrollTo({left: 0, top: document.body.scrollHeight, behavior: 'smooth'});
 }
 
+function remove_row(e) {
+    let rows = document.querySelectorAll('.flashcard-row');
+    if (rows.length > 1) {
+        let row = document.querySelector(`.flashcard-row[value='${e.currentTarget.value}']`);
+        row.remove();
+    }
+}
+
 add_row_btn.addEventListener('click', add_row);
 add_row_btn.addEventListener('click', scroll_smooth_to_bottom);
-remove_row_btn.addEventListener('click', remove_row);
+remove_btn.addEventListener('click', remove_row);
