d373eb731258414d979f62fcc360fd13740cb82a
Igor Szabłowski <iszablow@gmail.com>
2024-01-08 00:02:23 +0100

    removing row in creating collection

--- src/application/static/js-scripts/creating_flashcard.js
+++ src/application/static/js-scripts/creating_flashcard.js
@@ -1,6 +1,7 @@
 const add_row_btn = document.querySelector('#add-row-btn');
 const flashcard_row = document.querySelector('.flashcard-row');
 const all_flashcards = document.querySelector('.all-flashcards');
+const remove_row_btn = document.querySelector('#remove-row-btn');
 
 function clear_inputs(inputs) {
     inputs.forEach((input) => {
@@ -14,9 +15,17 @@ function add_row() {
     all_flashcards.appendChild(new_row);
 }
 
+function remove_row() {
+    let rows = document.querySelectorAll('.flashcard-row');
+    if (rows.length > 1) {
+        rows[rows.length-1].remove();
+    }
+}
+
 function scroll_smooth_to_bottom() {
     window.scrollTo({left: 0, top: document.body.scrollHeight, behavior: 'smooth'});
 }
 
 add_row_btn.addEventListener('click', add_row);
-add_row_btn.addEventListener('click', scroll_smooth_to_bottom)
+add_row_btn.addEventListener('click', scroll_smooth_to_bottom);
+remove_row_btn.addEventListener('click', remove_row);
