8c8bf295c47a6f4c983650acfecc5ff17f15b330
Igor Szabłowski <iszablow@gmail.com>
2024-02-05 19:36:26 +0100

    new database

--- src/application/__init__.py
+++ src/application/__init__.py
@@ -11,8 +11,11 @@ def create_app():
 
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = getenv('SECRET_KEY')
-    app.config['SQLALCHEMY_DATABASE_URI'] = getenv('SQLALCHEMY_DATABASE_URI')
+    # app.config['SECRET_KEY'] = getenv('SECRET_KEY')
+    app.config['SECRET_KEY'] = getenv('SECRET_KEY_PUBLIC_COLLECTIONS_BRANCHES')
+
+    # app.config['SQLALCHEMY_DATABASE_URI'] = getenv('SQLALCHEMY_DATABASE_URI')
+    app.config['SQLALCHEMY_DATABASE_URI'] = getenv('SQLALCHEMY_DATABASE_PUBLIC_COLLECTIONS_BRANCH_URI')
     app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=10)
 
     db.init_app(app)
